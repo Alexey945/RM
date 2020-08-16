15ff16a04cc6c519e50f48d547e849bd99c6883a
Алексей <Alekseyshaposnikov7@gmail.com>
2020-08-16 10:42:32 +0300

    Updates

---
+++ precache-manifest.bbc718931d065fb5ac310f7ee0a43a97.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bce0785be1ebdd9be8faa1b9b9ca35f",
+    "url": "/RM/index.html"
+  },
+  {
+    "revision": "9658b2db42943d9caf22",
+    "url": "/RM/static/css/2.ed87aa75.chunk.css"
+  },
+  {
+    "revision": "e7df28a1da530141444a",
+    "url": "/RM/static/css/main.073fc2d2.chunk.css"
+  },
+  {
+    "revision": "9658b2db42943d9caf22",
+    "url": "/RM/static/js/2.003c9920.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/RM/static/js/2.003c9920.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7df28a1da530141444a",
+    "url": "/RM/static/js/main.ad9e6b34.chunk.js"
+  },
+  {
+    "revision": "42c6e2ec68eab80b08f4",
+    "url": "/RM/static/js/runtime-main.1ddc6a19.js"
+  }
+]);
